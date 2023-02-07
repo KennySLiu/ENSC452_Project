9ca78596d935d8cf9cf10b6948a5fde23fda7ed5
Kenny Liu <ksl24@sfu.ca>
2023-02-06 18:39:42 -0800

    Fixing syntax issues in the testbench.

--- MY_PERSONAL_DESIGN/FFT_32Samples/FFT_32Samples.srcs/sim_1/new/fft_testbench.vhd
+++ MY_PERSONAL_DESIGN/FFT_32Samples/FFT_32Samples.srcs/sim_1/new/fft_testbench.vhd
@@ -23,6 +23,8 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.Numeric_STD.all;
+
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -85,32 +87,42 @@ architecture Behavioral of fft_testbench is
     signal clk_48k : std_logic;
     signal clk_200m : std_logic;
 
+    ----------------------------------------
+    -- INPUT DATA:
+    -- 2 channels, 24-bits each, because we configured our block with 24-bit input, and 2 channels.
+    signal IN_DATA_CH1_RE   : STD_LOGIC_VECTOR(23 downto 0) := (others => '0');
+    signal IN_DATA_CH1_IM   : STD_LOGIC_VECTOR(23 downto 0) := (others => '0');
+    signal IN_DATA_CH2_RE   : STD_LOGIC_VECTOR(23 downto 0) := (others => '0');
+    signal IN_DATA_CH2_IM   : STD_LOGIC_VECTOR(23 downto 0) := (others => '0');
 
-    signal M_AXIS_DATA_0_tdata                 : out STD_LOGIC_VECTOR ( 95 downto 0 );
-    signal M_AXIS_DATA_0_tlast                 : out STD_LOGIC;
-    signal M_AXIS_DATA_0_tready                : in STD_LOGIC;
-    signal M_AXIS_DATA_0_tvalid                : out STD_LOGIC;
-    signal S_AXIS_CONFIG_0_tdata               : in STD_LOGIC_VECTOR ( 23 downto 0 );
-    signal S_AXIS_CONFIG_0_tready              : out STD_LOGIC;
-    signal S_AXIS_CONFIG_0_tvalid              : in STD_LOGIC;
-    signal S_AXIS_DATA_0_tdata                 : in STD_LOGIC_VECTOR ( 95 downto 0 );
-    signal S_AXIS_DATA_0_tlast                 : in STD_LOGIC;
-    signal S_AXIS_DATA_0_tready                : out STD_LOGIC;
-    signal S_AXIS_DATA_0_tvalid                : in STD_LOGIC;
-    --signal aclk_0                              : in STD_LOGIC;
-    signal aclken_0                            : in STD_LOGIC;
-    signal aresetn_0                           : in STD_LOGIC;
-    signal event_data_in_channel_halt_0        : out STD_LOGIC;
-    signal event_data_out_channel_halt_0       : out STD_LOGIC;
-    signal event_frame_started_0               : out STD_LOGIC;
-    signal event_status_channel_halt_0         : out STD_LOGIC;
-    signal event_tlast_missing_0               : out STD_LOGIC;
-    signal event_tlast_unexpected_0            : out STD_LOGIC
+    ----------------------------------------
+    -- SIGNALS to connect directly to the FFT block:
+    ----------------------------------------
+    --signal aclk_0                              : in STD_LOGIC := '0';
+    signal M_AXIS_DATA_0_tdata                 : STD_LOGIC_VECTOR ( 95 downto 0 )   := (others => '0');
+    signal S_AXIS_CONFIG_0_tdata               : STD_LOGIC_VECTOR ( 23 downto 0 )   := (others => '0');
+    signal S_AXIS_DATA_0_tdata                 : STD_LOGIC_VECTOR ( 95 downto 0 )   := (others => '0');
+    signal M_AXIS_DATA_0_tlast                 : STD_LOGIC                          := '0';
+    signal M_AXIS_DATA_0_tready                : STD_LOGIC                          := '0';
+    signal M_AXIS_DATA_0_tvalid                : STD_LOGIC                          := '0';
+    signal S_AXIS_CONFIG_0_tready              : STD_LOGIC                          := '0';
+    signal S_AXIS_CONFIG_0_tvalid              : STD_LOGIC                          := '0';
+    signal S_AXIS_DATA_0_tlast                 : STD_LOGIC                          := '0';
+    signal S_AXIS_DATA_0_tready                : STD_LOGIC                          := '0';
+    signal S_AXIS_DATA_0_tvalid                : STD_LOGIC                          := '0';
+    signal aclken_0                            : STD_LOGIC                          := '0';
+    signal aresetn_0                           : STD_LOGIC                          := '0';
+    signal event_data_in_channel_halt_0        : STD_LOGIC                          := '0';
+    signal event_data_out_channel_halt_0       : STD_LOGIC                          := '0';
+    signal event_frame_started_0               : STD_LOGIC                          := '0';
+    signal event_status_channel_halt_0         : STD_LOGIC                          := '0';
+    signal event_tlast_missing_0               : STD_LOGIC                          := '0';
+    signal event_tlast_unexpected_0            : STD_LOGIC                          := '0';
 
 
 begin
 
-    clk_gen(clk_48k, 48000);
+    clk_gen(clk_48k, 48000.0);
     clk_gen(clk_200m, 200.00000e6);
 
     FFT_BLK : xil_fft port map(
@@ -137,8 +149,27 @@ begin
     );
     
     
-    DC_Stimulus: process
+    DC_Stimulus: process(clk_200m, clk_48k)
+        --variable clk_48k_just_rose : integer range 0 to 1;
+        variable clk_ctr : integer range 0 to 10000000;
     begin
+        -- Don't bother simulating complex inputs - only real inputs allowed!
+        IN_DATA_CH1_IM <= (others => '0');
+        IN_DATA_CH2_IM <= (others => '0');
+
+        if rising_edge(clk_200m) then
+            clk_ctr := clk_ctr + 1;
+            IN_DATA_CH1_RE <= std_logic_vector(to_signed(5000, IN_DATA_CH1_RE'length));
+            IN_DATA_CH2_RE <= std_logic_vector(to_signed(5000, IN_DATA_CH2_RE'length));
+
+            S_AXIS_DATA_0_tdata <= IN_DATA_CH2_IM &
+                                   IN_DATA_CH2_RE &
+                                   IN_DATA_CH1_IM & 
+                                   IN_DATA_CH1_RE;
+
+            S_AXIS_DATA_0_tvalid <= '1';
+ 
+        end if;
     end process;
 
 
